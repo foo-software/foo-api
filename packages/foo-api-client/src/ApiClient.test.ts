2144dac83c9639c20d42b68c4685f86e002602c1
adamhenson <adamhenson1979@gmail.com>
2021-05-03 18:11:03 -0400

    chore: update name

--- packages/foo-api-client/src/ApiClient.test.ts
+++ packages/foo-api-client/src/ApiClient.test.ts
@@ -1,7 +1,7 @@
 import ApiClient from './ApiClient';
 
 describe('ApiClient', () => {
-  it('throws an error when incorrectly instantiated', () => {
+  it('throws error when incorrectly instantiated', () => {
     expect(() => new ApiClient({ apiToken: '' })).toThrow(
       'API token is missing.'
     );
