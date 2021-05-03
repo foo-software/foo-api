605bca47047a0c16af4f36e29f21369c2bd6f9d4
adamhenson <adamhenson1979@gmail.com>
2021-05-03 13:23:53 -0400

    feat: initial endpoints

---
+++ packages/foo-api-client/src/createPageQueueItem.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+import { LATEST_API_URL } from './constants';
+import {
+  CreatePageQueueItemPayloadInterface,
+  PageParameters,
+  QueueItemApiResponseInterface,
+} from './interfaces';
+
+export default async ({
+  parameters: { apiToken, apiUrl = LATEST_API_URL, id },
+  payload,
+}: {
+  parameters: PageParameters;
+  payload: CreatePageQueueItemPayloadInterface;
+}): Promise<QueueItemApiResponseInterface> => {
+  const result = await fetch(`${apiUrl}/pages/${id}/queueItems`, {
+    method: 'post',
+    headers: {
+      authorization: apiToken,
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+  return result.json();
+};
