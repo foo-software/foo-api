f310a3ba5fe1dc6aab0f225d20bb65faa1de00d8
adamhenson <adamhenson1979@gmail.com>
2021-09-04 11:01:41 -0400

    feat: web vitals tracking

--- packages/foo-api-docs/src/pages/index.js
+++ packages/foo-api-docs/src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
@@ -28,13 +29,51 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <>
+      <Head>
+        <script defer src="https://unpkg.com/web-vitals"></script>
+        <script>{`
+          const VITALS_ACCOUNT_ID = '613382df759af90024b205e1';
+          const VITALS_URL = 'https://www.foo.software/api/vitals';
+
+          function postVitals(metric) {
+            const body = JSON.stringify({
+              accountId: VITALS_ACCOUNT_ID,
+              name: metric.name,
+              url: window.location.href,
+              value: metric.value,
+            });
+
+            return (
+              (navigator.sendBeacon && navigator.sendBeacon(VITALS_URL, body)) ||
+              fetch(VITALS_URL, {
+                body,
+                method: 'POST',
+                keepalive: true,
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              })
+            );
+          };
+
+          addEventListener('DOMContentLoaded', function() {
+            webVitals.getCLS(postVitals);
+            webVitals.getFCP(postVitals);
+            webVitals.getFID(postVitals);
+            webVitals.getLCP(postVitals);
+            webVitals.getTTFB(postVitals);
+          });
+        `}</script>
+      </Head>
+      <Layout
+        title={`Hello from ${siteConfig.title}`}
+        description="Description will go into a meta tag in <head />">
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </Layout>
+    </>
   );
 }
